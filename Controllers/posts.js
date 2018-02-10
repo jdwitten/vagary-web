4939aa5eb0cad14ec4b770b9306660c9f7bb0af8
Jonathan Witten <jonathanwitten@willowtreeapps.com>
2018-02-09 23:45:37 -0500

    update compose file and test redeployment

--- Controllers/posts.js
+++ Controllers/posts.js
@@ -3,7 +3,7 @@ var router = express.Router()
 var Post = require('../Models/post.js')
 
 router.get('/', function (req, res) {
-  var post = new Post(1, 1, "Backpacking the Balkans", [{url: "https://www.gstatic.com/webp/gallery/1.jpg"}], {id : 1,title: "Backpacking the Balkans",posts : [1, 2, 3]}, "Croatia")
+  var post = new Post(1, 1, "Backpacking the Balkans", [{url: "https://www.gstatic.com/webp/gallery/1.jpg"}], {id : 555555,title: "Backpacking the Balkans",posts : [1, 2, 3]}, "Croatia")
   res.send({posts: [{id: post.id, title: post.title, author: post.author, content: post.content, trip: post.trip, location: post.location }]})
 })
 
